e33600f5aeedbd208aac650c992f6683ec354479
Raul Chetan <raul.devden@gmail.com>
2023-02-09 23:52:10 +0100

    added load images from folder script and masonry layout grid display

--- scripts/image_loader.js
+++ scripts/image_loader.js
@@ -7,5 +7,25 @@ document.addEventListener("DOMContentLoaded", function(){
     let imageDisplayHTML = document.querySelector('#image-display');
 
     console.log(imageDisplayHTML);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/assets/kevinsacris_0", true);
+    xhr.responseType = 'document';
+    xhr.onload = () => {
+    if (xhr.status === 200) {
+        var elements = xhr.response.getElementsByTagName("a");
+        for (x of elements) {
+        if ( x.href.match(/\.(jpe?g|png|gif)$/) ) { 
+            let img = document.createElement("img");
+            img.src = x.href;
+            imageDisplayHTML.appendChild(img);
+        } 
+        };
+    } 
+    else {
+        alert('Request failed. Returned status of ' + xhr.status);
+    }
+    }
+    xhr.send()
     
 });
