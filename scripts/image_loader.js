25e5082a46ca1bd102d3f1e7c52d6037be78f614
Raul Chetan <raul.devden@gmail.com>
2023-02-13 23:56:51 +0100

    added script to load folders

--- scripts/image_loader.js
+++ scripts/image_loader.js
@@ -4,28 +4,59 @@ document.onload = ()=>{
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-    let imageDisplayHTML = document.querySelector('#image-display');
+    let imageDisplayHTML = document.querySelector('#gallery-display');
 
     console.log(imageDisplayHTML);
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/assets/kevinsacris_0", true);
-    xhr.responseType = 'document';
-    xhr.onload = () => {
-    if (xhr.status === 200) {
-        var elements = xhr.response.getElementsByTagName("a");
-        for (x of elements) {
-        if ( x.href.match(/\.(jpe?g|png|gif)$/) ) { 
-            let img = document.createElement("img");
-            img.src = x.href;
-            imageDisplayHTML.appendChild(img);
+    loadGallery('morenodiego_0');
+
+    loadMainMenu();
+
+    function loadMainMenu(){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", `/assets`, true);
+        xhr.responseType = 'document';
+        xhr.onload = () => {
+        if (xhr.status === 200) {
+            console.log(xhr);
+            var elements = xhr.response.getElementsByTagName("a");
+            // console.log(elements);
+            for (x of elements) {
+                console.log(x.title);
+            if ( x.href.match(/\.(jpe?g|png|gif)$/) ) { 
+                let img = document.createElement("img");
+                img.src = x.href;
+                imageDisplayHTML.appendChild(img);
+            } 
+            };
         } 
-        };
-    } 
-    else {
-        alert('Request failed. Returned status of ' + xhr.status);
+        else {
+            alert('Request failed. Returned status of ' + xhr.status);
+        }
+        }
+        xhr.send()
     }
+
+    function loadGallery(galleryName){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", `/assets/${galleryName}`, true);
+        xhr.responseType = 'document';
+        xhr.onload = () => {
+        if (xhr.status === 200) {
+            var elements = xhr.response.getElementsByTagName("a");
+            for (x of elements) {
+            if ( x.href.match(/\.(jpe?g|png|gif)$/) ) { 
+                let img = document.createElement("img");
+                img.src = x.href;
+                imageDisplayHTML.appendChild(img);
+            } 
+            };
+        } 
+        else {
+            alert('Request failed. Returned status of ' + xhr.status);
+        }
+        }
+        xhr.send()
     }
-    xhr.send()
     
 });
